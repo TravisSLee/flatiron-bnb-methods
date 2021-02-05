e2b1021b0641b7aa757b5153fd9b80417b14c7e2
TravisSLee <travisslee223@gmail.com>
2021-02-05 14:47:26 -0500

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,60 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  def city_openings(start_date, end_date)
+    availble_listings = []
+    cities = City.all
+    cities.each do |c|
+      c.listings.each do |l|
+          l.reservations.each do |r|
+            if r.checkin <= start_date.to_date  && r.checkout >= end_date.to_date 
+              availble_listings << l
+            end
+          end
+      end
+    end
+    availble_listings
+  end
 
+  def self.highest_ratio_res_to_listings
+    
+    cities = self.all
+    highest_city_ratio = 0
+    highest_city_id = 0
+
+    cities.each do |c|
+      listing_count = c.listings.count
+      reservation_count = 0
+      c.listings.each do |listing|
+        reservation_count += listing.reservations.count
+      end
+
+      ratio = reservation_count.to_f / listing_count.to_f
+
+      if ratio > highest_city_ratio
+        highest_city_id = c.id
+        highest_city_ratio = ratio
+      end
+    end
+    self.find(highest_city_id)
+  end
+
+  def self.most_res
+    cities = self.all
+    highest_city_reservation = 0
+    highest_city_id = 0
+
+    cities.each do |c|
+      listing_count = c.listings.count
+      reservation_count = 0
+      c.listings.each do |listing|
+        reservation_count += listing.reservations.count
+      end
+      if reservation_count > highest_city_reservation
+        highest_city_id = c.id
+        highest_city_reservation = reservation_count
+      end
+    end
+    self.find(highest_city_id)
+  end
 end
-
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,21 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true
+
+  def average_review_rating
+    listing = Listing.find(id)
+    review_count = 0
+    review_rating_total = 0
+
+    
+      listing.reservations.each do |res|
+        if res.review
+        review_count += 1
+        review_rating_total += res.review.rating
+        end
+      end
+    average_rating = review_rating_total.to_f / review_count.to_f
+    average_rating 
+  end
 end
